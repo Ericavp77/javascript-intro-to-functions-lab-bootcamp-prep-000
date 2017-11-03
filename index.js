705a09b05f6d41a27bf960ce2726844a884f1374
Erica Pitts <ericavp77@gmail.com>
2017-11-03 15:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 var sayHiToGrandma = function (string) {
+
   if (string.toLowerCase()) {
 return "I can't hear you!"
-}
- if (string.toUpperCase()) {
+
+} else  (string.toUpperCase()) {
   return "YES INDEED!"
-}
-  if (sayHiToGrandma === "I love you, Grandma.") {
+
+} else (sayHiToGrandma === "I love you, Grandma.")
   return "I love you, too."
 }
